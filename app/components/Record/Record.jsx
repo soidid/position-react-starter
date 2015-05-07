ce12d833233349dac5bfb295ed50d970d2943f12
soidid <soidid@gmail.com>
2015-05-07 13:19:54 +0800

    add 'read more' when quote is too long (limit:140)

--- app/components/Record/Record.jsx
+++ app/components/Record/Record.jsx
@@ -10,6 +10,7 @@ import "./Record.css";
 export default class Record extends React.Component {
   constructor(props) { super(props)
       this.state = { 
+          showTooltip: false,
           showFull: false
       }
   }
@@ -22,10 +23,18 @@ export default class Record extends React.Component {
       
   }
 
+  _onToggleShowTooltip(){
+
+      this.setState({ 
+          showTooltip: !this.state.showTooltip
+      });
+      
+  }
+
   render() {
    
     var {data} = this.props;
-    var {showFull} = this.state;
+    var {showTooltip, showFull} = this.state;
     
     var opinionClasses = classNames({
       "Record-opinion": true,
@@ -34,27 +43,38 @@ export default class Record extends React.Component {
       "is-unclear": data.opinion === '不明確'
     });
    
+    var bindToggleShowTooltip = this._onToggleShowTooltip.bind(this, null);
     var bindToggleShowFull = this._onToggleShowFull.bind(this, null);
     
-    var tooltip = (showFull) ? 
+    var tooltip = (showTooltip) ? 
         <div className="Record-tooltip">
             <div className="Record-tooltipItem">複製連結</div>
             <div className="Record-tooltipItem">看立法院完整紀錄</div>
             <div className="Record-tooltipItem">檢舉</div>
         </div> : ""; 
     
+    var quoteContent = data.quote;
+    if(!showFull && data.quote.length > 140)
+        quoteContent = data.quote.substring(0,140);
+
+    var readMoreSpan = (!showFull && data.quote.length > 140) ?
+    <span className="Reocrd-readFull"> ...閱讀全部</span>:"";
+
     return (
         <div className="Record">
             <div className="Record-title">
                 <LegislatorAvatar data={data.name} name={true}/>
                 <div className={opinionClasses}>{data.opinion}</div>
             </div>
-            <div className="Record-quote">
-                {data.quote}
+            <div className="Record-quote"
+                 onClick={bindToggleShowFull}>
+                 {quoteContent}
+                 {readMoreSpan}
             </div>
             <div className="Record-info">
                 －{data.type}，{data.date}
             </div>
+           
             <div className="Record-action">
                 <div className="Record-actionItem">
                     <span className="Record-star"><Icon icon={"star"}/> {data.trustVote}</span>  
@@ -64,7 +84,7 @@ export default class Record extends React.Component {
                 </div>
                 <div className="Record-actionItem">
                     <span className="Record-more"
-                          onClick={bindToggleShowFull}><Icon icon={"ellipsis-h"}/> 
+                          onClick={bindToggleShowTooltip}><Icon icon={"ellipsis-h"}/> 
                     </span>
                     {tooltip}
                 </div>
